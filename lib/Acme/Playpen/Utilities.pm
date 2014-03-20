e8cdac9e56fceb0a823eee2e713ae05e1bfe6ba0
Neil Bowers <neil@bowers.com>
2014-03-20 16:56:31 +0000

    Import of NEILB/Acme-Playpen-0.14 from CPAN.
    gitpan-cpan-distribution: Acme-Playpen
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         NEILB/Acme-Playpen-0.14.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

--- lib/Acme/Playpen/Utilities.pm
+++ lib/Acme/Playpen/Utilities.pm
@@ -1,6 +1,6 @@
 package Acme::Playpen::Utilities;
 
-our $VERSION = '0.12_01';
+our $VERSION = '0.12_02';
 
 use strict;
 use warnings;
@@ -20,6 +20,8 @@ Acme::Playpen::Utilities - I'm just this module, you know
  # do nothing
 
 =head1 DESCRIPTION
+
+Danger Will Robinson! This is a developer version -- don't install it unless you're sure!
 
 Acme::Playpen::Utilities is a nothing little module,
 used while i'm checking various things related to the CPAN toolchain.
